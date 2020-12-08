13af73b3c13e495ff724a06bd375a355b8fabd73
nurkholis1205 <75239787+nurkholis1205@users.noreply.github.com>
2020-12-08 15:34:49 +0700

    Add files via upload

---
+++ data/KELOMPOK4_1.js
@@ -0,0 +1,10 @@
+var json_KELOMPOK4_1 = {
+"type": "FeatureCollection",
+"name": "KELOMPOK4_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "Point", "coordinates": [ 110.848801182118379, -6.797664164321255 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 110.848801182118379, -6.79758607642593 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.841408955674027, -6.817029570303895 ] } }
+]
+}
